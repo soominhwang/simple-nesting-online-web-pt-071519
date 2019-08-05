97813cb3fe6048ff1687ae1b8f2c52a24bab2ddb
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-05 05:04:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash[:yukihiro_matsumoto] =
-      {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-       }
+  
 
 	programmer_hash =
  		{
@@ -80,6 +76,13 @@ def adding_matz
           :languages => ["C"]
         }
       }
+
+    programmer_hash[:yukihiro_matsumoto] =
+        {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+         }
+    programmer_hash
 
 end
 
