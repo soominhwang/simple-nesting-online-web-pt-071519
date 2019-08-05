6fa904249c0d31c7f59e9f8868067cb6a658aa4b
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-05 04:48:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,7 +73,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-
+      }
   programmer_hash[:yukihiro_matsumoto] =
       {
         :known_for => "Ruby",
