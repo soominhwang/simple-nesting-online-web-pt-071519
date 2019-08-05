78cf30a53b402fe5abfcb02a44e0fe268160a440
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-05 04:47:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,10 +74,10 @@ def adding_matz
           :languages => ["C"]
         }
 
-     programmer_hash[:yukihiro_matsumoto] =>
-       {
+  programmer_hash[:yukihiro_matsumoto] =
+      {
         :known_for => "Ruby",
-         :languages => ["LISP", "C"]
+        :languages => ["LISP", "C"]
        }
      }
    }
